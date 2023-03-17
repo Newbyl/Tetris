eaaadf29be466314a2faf5fb30ab204366da052b
Ragavan Mugilan <mugilan.ragavan@etu.univ-orleans.fr>
2023-03-17 11:04:40 +0100

    add Grille.java

---
+++ app/src/main/java/com/example/tetris/Grille.java
@@ -0,0 +1,45 @@
+package com.example.tetris;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Grille {
+    private ArrayList< ArrayList<Integer>> grille;
+
+
+
+    public Grille(int x , int y) {
+        grille = new ArrayList<>(y);
+        for (int i = 0; i < y; i++) {
+            ArrayList<Integer> l = new ArrayList<>(x);
+            for (int j = 0; j < x; j++) {
+                l.add(0);
+            }
+            this.grille.add(l);
+        }
+        ArrayList<Integer> l = new ArrayList<>();
+        l.add(1);
+        l.add(1);
+        l.add(0);
+        l.add(1);
+        l.add(0);
+        l.add(1);
+        l.add(1);
+        grille.set(0,l);
+        System.out.println(grille);
+    }
+
+
+    public int Get(int x , int y){
+        return this.grille.get(y).get(x);
+    }
+
+    public void Test(){
+        ArrayList<Integer> l;
+        l = this.grille.get(6);
+        this.grille.remove(6);
+        this.grille.add(0,l);
+    }
+}
